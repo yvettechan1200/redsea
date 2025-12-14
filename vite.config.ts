b7e477d9c27349b7c8d2ada75e87ef40832596a8
yvettechan1200 <minio121086@gmail.com>
2025-12-15 00:57:33 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
   plugins: [react()],
-  base: "/red-sea/",  // <--- 加上這一行
+  base: "/redsea/",  // <--- 加上這一行
 })
     const env = loadEnv(mode, '.', '');
     return {
